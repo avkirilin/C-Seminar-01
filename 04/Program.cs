274dc486bda1e6a1ac1990a11d63c5423be4bda7
avkirilin <nerosashok@mail.ru>
2022-12-18 15:53:41 +0300

    Написана програма для решения второй домашней задачи

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+int max = firstNum;
+if (max < secondNum)
+{
+    max = secondNum;
+}
+else
+{
+    
+}
+if (max < thirdNum)
+{
+    max = thirdNum;
+}
+Console.Write("Наибольшее из трех чисел: ");
+Console.WriteLine(max);
