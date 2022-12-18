3a3145a39bf42ab1601649aef928ad5f7d875734
avkirilin <nerosashok@mail.ru>
2022-12-18 15:33:25 +0300

    Написали первую программу

--- 02/Program.cs
+++ 02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+if (firstNum > secondNum)
+{
+    Console.Write("Наибольшее из двух чисел: ");
+    Console.Write(firstNum);
+}
+else
+{
+    Console.Write("Наибольшее из двух чисел: ");
+    Console.Write(secondNum);
+}
