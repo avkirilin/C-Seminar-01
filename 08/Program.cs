3133fe2d9c807152d7b1eb8edb2449a304f09661
avkirilin <nerosashok@mail.ru>
2022-12-18 16:32:48 +0300

    Написана программа для решения последней домашней задачи

--- 08/Program.cs
+++ 08/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = 1;
+Console.Write("Все четные числа от 1 до ");
+Console.WriteLine(N);
+while (A < N + 1)
+{
+    if (A % 2 == 0)
+    {
+        Console.Write(A);
+        Console.Write(", ");
+    }
+    else
+    {
+
+    }
+    A = A + 1;
+}
