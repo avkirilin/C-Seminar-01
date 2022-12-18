236881ba394fbfab5636e16bfb003b19bddd5320
avkirilin <nerosashok@mail.ru>
2022-12-18 16:05:20 +0300

    Написана программа для решения третьей домашней задачи

--- 06/Program.cs
+++ 06/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число для проверки");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A % 2 == 0)
+{
+    Console.Write("Данное число является четным");
+}
+else
+{
+    Console.Write("Данное число является нечетным");
+}
